556e7b50dbf7ca5c518a975218592d401e0049ca
Riley Westbrook <westbrookriley@gmail.com>
2017-10-22 18:41:36 -0400

    messages not appearing

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,17 +1,25 @@
 (function() {
-  function HomeCtrl(Room, $uibModal) {
-    this.rooms = Room.all;
+  function HomeCtrl(Room, Message, $uibModal) {
+    var home = this;
+    home.rooms = Room.all;
+    home.currentRoom = null;
 
-    this.addRoom = function() {
+    home.addRoom = function() {
       $uibModal.open({
         templateUrl: '/templates/modal.html',
         size: 'sm',
         controller: 'ModalCtrl as modal'
       });
     }
+
+    home.setCurrentRoom = function (room) {
+      home.currentRoom = room;
+      home.messages = Message.getByRoomId(home.currentRoom.$id);
+      console.log(home.messages);
+    }
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
