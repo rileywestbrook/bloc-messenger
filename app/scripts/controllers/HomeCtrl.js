5aaa285a2cd08aea9459d71aeb65d4b88d01354f
Riley Westbrook <westbrookriley@gmail.com>
2017-10-26 18:11:52 -0400

    Messages added, styling for message bar still needed:

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-  function HomeCtrl(Room, Message, $uibModal) {
+  function HomeCtrl(Room, Message, $uibModal, $cookies) {
     var home = this;
     home.rooms = Room.all;
     home.currentRoom = null;
+    home.currentUser = $cookies.get('blocChatCurrentUser');
 
     home.addRoom = function() {
       $uibModal.open({
@@ -16,9 +17,15 @@
       home.currentRoom = room;
       home.messages = Message.getByRoomId(home.currentRoom.$id);
     }
+
+    home.sendMessage = function () {
+      home.newMessage.roomId = home.currentRoom.$id;
+      home.newMessage.username = home.currentUser;
+      Message.send(home.newMessage);
+    }
   }
 
   angular
     .module('blocChat')
-    .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
+    .controller('HomeCtrl', ['Room', 'Message', '$uibModal', '$cookies', HomeCtrl]);
 })();
